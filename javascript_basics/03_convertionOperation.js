82a955f0a3c781787104788701dcf1a245503c93
SurajVishw <131581678+SurajVishw@users.noreply.github.com>
2024-08-25 11:00:48 +0000

    datatypes

---
+++ javascript_basics/03_convertionOperation.js
@@ -0,0 +1,24 @@
+let score = "Suraj";
+
+let scoreNumber= Number(score);
+
+// console.log(score);
+// console.log(scoreNumber);
+// console.log(typeof(scoreNumber));
+
+// string => NaN
+// number with string => Nan
+
+let isLoggedIn="Suraj";
+let booleanValue=Boolean(isLoggedIn);
+console.log(typeof isLoggedIn)
+console.log(booleanValue);
+
+// 1=>true: 0=>false
+// empty string false : string => true
+
+let userName="Suraj"
+let stringName=String(userName);
+
+// console.log(typeof userName);
+// console.log(typeof stringName);

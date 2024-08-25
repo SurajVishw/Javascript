8bb512b82f99a1f4a6d2df299e4cc1ec2b54555d
SurajVishw <131581678+SurajVishw@users.noreply.github.com>
2024-08-25 10:30:55 +0000

    javascript datatypes

---
+++ javascript_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=123456;
+let accountEmail="suraj@gmail.com";
+var accountPassword="786786";
+accountCity="Jaipur";
+let accountState;
+// accountId=2;
+
+accountEmail="suraj1@gmail.com";
+accountPassword="5555555";
+accountCity="Mumbai";
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
---
+++ javascript_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name="Suraj";
+let age=24;
+let isLoggedIn=true;
+
+// Data Types 
+
+// number => 2 to power 53
+// strong => ""
+// bigint => Larger Number
+// boolean => true/false
+// null => empty value
+// undefined => value is not assigned to variable
+// symbol
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
